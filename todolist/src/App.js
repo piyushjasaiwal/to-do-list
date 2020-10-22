9002e97370d022ddb1d2d64be3ad643e72a7400f
Piyush Jasaiwal <coderpiyush123@gmail.com>
2020-10-22 14:00:25 +0530

    made changes

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ListItems from './ListItem.js'
 
@@ -17,6 +17,21 @@ class App extends React.Component{
     this.addItem = this.addItem.bind(this);
   }
 
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !== ""){
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items:items,
+        currentItem : {
+          text:'',
+          key:''
+        }
+      }) 
+    }
+  }
+
   handleInput(e){
     this.setState({
       currentItem:{
@@ -24,22 +39,8 @@ class App extends React.Component{
         key : Date.now()
       }
     })
-  }
-
-  addItem(e){
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    if(newItem.text !== ""){}
-      const newItems = [...this.state.items, newItem];
-      this.setState({
-        items:newItems,
-        currentItem : {
-          text:'',
-          key:''
-        }
-      })
-  }
-  
+    // console.log(this.state.currentItem);
+  }  
 
 
   render(){
@@ -47,12 +48,13 @@ class App extends React.Component{
       <div>
         <header>
         <form onSubmit = {this.addItem}>
-          <input type = "text" placeholder = "Enter text" value = {this.state.currentItem.text} onChange = {this.handleInput}></input>
+          <input type = "text" placeholder = "Enter text" value = {this.state.currentItem.text}onChange = {this.handleInput}></input>
           <button type = "submit">
             ADD
           </button>
         </form>
       </header>
+      <p>{this.state.items.text}</p>
         <ListItems items = {this.state.items}></ListItems>
       </div>
     );
